4452420f7078d39e9a57e5de9d9102ddbeb65053
deepak-softflam <deepak.kale@softflam.in>
2021-02-23 20:36:42 +0530

    added router file

---
+++ Router/index.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var router = express.Router()
+const user = require('../Controller/User')
+const moment = require('../Controller/Moment')
+const multer= require('multer')
+const auth = require('../Auth/auth')
+
+// initialize multer storage path 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'tmp')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+  var upload = multer({ storage: storage })
+
+// login api route
+router.post('/login',user.login)
+router.post('/newUser', user.addUser)
+
+// moment api route 
+router.post('/addMoment',auth,upload.array('img','12') ,moment.AddMoment)
+router.get('/getMomentList',auth,moment.GetMomentList)
+router.put('/updateMoment/:id',auth,upload.array('img','12'),moment.UpdateMoment)
+router.delete('/deleteMoment/:id',auth,moment.DeleteMoment)
+
+
+//other
+router.post('/addUser',user.addUser)
+
+module.exports = router ;
